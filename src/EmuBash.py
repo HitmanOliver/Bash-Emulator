971a9c5dce713df977481dc71580876b63e45ba0
HitmanOliver <kodzoevsultan19@gmail.com>
2026-09-21 17:37:20 +0300

    style: fix comment typos

--- src/EmuBash.py
+++ src/EmuBash.py
@@ -3,7 +3,7 @@ import os
 from tkinter import ttk
 
 
-# Константы
+"""Константы"""
 T_WINDOW_W = 1000
 T_WINDOW_H = 800
 T_WINDOW_PAD_X = 50
@@ -12,7 +12,7 @@ INPUT_PAD_X = 8
 INPUT_PAD_Y = 8
 COMMANDS = ['ls', 'cd', 'exit']
 
-# Коды результата обработки команды
+"""Коды результата обработки команды"""
 CODE_SUCCESS = 0
 CODE_INVALID_COMMAND = 1
 CODE_INVALID_ARGUMENT = 2
